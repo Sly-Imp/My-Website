44930e3b0b204ba8ee23a74b55f8df4aa8778cee
Sly-Imp <khatiwadaprince1@gmail.com>
2022-11-14 20:38:15 +0545

    Version_18.0

---
+++ public/main.js
@@ -0,0 +1,17 @@
+const myNav = document.getElementsByClassName('fnav')[0];
+const goUp = document.getElementsByClassName('goup')[0];
+
+window.onscroll = ()=>{
+    if(window.scrollY >= 100 && window.scrollY <= 1050){
+        myNav.style.right='4rem';
+    }
+    else{
+        myNav.style.right='-5rem';
+    }
+    if(window.scrollY >=1200){
+        goUp.style.bottom='1rem';
+    }
+    else{
+        goUp.style.bottom='-3rem';
+    }
+};
