7ca981097d1431851c68d823543284520ffc9219
Sly-Imp <khatiwadaprince1@gmail.com>
2023-01-09 21:32:50 +0545

    Patch

--- public/main.js
+++ public/main.js
@@ -2,13 +2,13 @@ const myNav = document.getElementsByClassName('fnav')[0];
 const goUp = document.getElementsByClassName('goup')[0];
 
 window.onscroll = ()=>{
-    if(window.scrollY >= 100 && window.scrollY <= 950){
+    if(window.scrollY >= 100 && window.scrollY <= 2169){
         myNav.style.right='4rem';
     }
     else{
         myNav.style.right='-5rem';
     }
-    if(window.scrollY >=1200){
+    if(window.scrollY >=2581){
         goUp.style.bottom='1rem';
     }
     else{
