139766082cf467ddd021d8b27cb8659ba813087a
Sly-Imp <khatiwadaprince1@gmail.com>
2023-01-11 18:50:44 +0545

    Website 3.0

--- public/main.js
+++ public/main.js
@@ -1,17 +1,25 @@
-const myNav = document.getElementsByClassName('ftitle-bar')[0];
+const myNav = document.getElementsByClassName('fnav')[0];
 const goUp = document.getElementsByClassName('goup')[0];
+const bars = document.querySelectorAll('.anim');
+const width = [95, 80, 65, 20, 25, 5]
+
 
 window.onscroll = ()=>{
     if(window.scrollY >= 705){
-        myNav.style.display='flex';
+        myNav.style.left='0';
     }
     else{
-        myNav.style.display='none';
+        myNav.style.left='-6rem';
     }
     if(window.scrollY >=4312){
         goUp.style.bottom='1rem';
     }
     else{
         goUp.style.bottom='-3rem';
+    }
+    if(window.scrollY >= 1300){
+        for (var i=0;i<=5;i++){
+            bars[i].style.width=`${width[i]}%`;
+        };
     }
 };
